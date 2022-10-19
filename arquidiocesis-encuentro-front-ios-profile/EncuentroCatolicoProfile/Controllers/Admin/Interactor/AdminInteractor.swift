d0a12c491253efff9087e8ebfce7cd581da3f121
For Linko <forlinko@4shl3y.local>
2022-10-19 17:59:46 -0500

    chage logic for OTP message handler, change interactor service for delete user

--- arquidiocesis-encuentro-front-ios-profile/EncuentroCatolicoProfile/Controllers/Admin/Interactor/AdminInteractor.swift
+++ arquidiocesis-encuentro-front-ios-profile/EncuentroCatolicoProfile/Controllers/Admin/Interactor/AdminInteractor.swift
@@ -20,35 +20,42 @@ class AdminInteractor: ProtocolosAdminInteractorInput {
         
     }
     func requestDeleteColaborador(id: Int?, email: String?, phone: String?) {
-            guard let email = email,
-                   let apiURL = URL(string: "\(APIType.shared.Auth())/user/delete?email=\(email)")
-            else {
-                self.presenter?.responseDeleteColaborador(status: false)
+        guard let apiURL = URL(string: "\(APIType.shared.Auth())/user/delete")
+        else {
+            self.presenter?.responseDeleteColaborador(status: false)
+            return
+        }
+        let defaults = UserDefaults.standard
+        let tksession = defaults.string(forKey: "idToken")
+        let parameterDictionary: [String : Any] = [
+            "user_id" :  defaults.string(forKey: "id") ?? ""
+        ]
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameterDictionary, options: []) else {
+            return
+        }
+        var request = URLRequest(url: apiURL)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \( tksession ?? "")", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "DELETE"
+        request.httpBody = httpBody
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            //print("->  request 🤡: ", request as Any)
+            //print("->  respuesta Status Code: ", response as Any)
+            //print("->  error: ", error as Any)
+            guard let allData = data else { return }
+            let outputStr  = String(data: allData, encoding: String.Encoding.utf8) as String?
+            //print("-->✅  Response ->  ", outputStr as Any)
+            if error != nil {
+                print("Hubo un error")
                 return
             }
-            var request = URLRequest(url: apiURL)
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            let tksession = UserDefaults.standard.string(forKey: "idToken")
-            request.setValue("Bearer \( tksession ?? "")", forHTTPHeaderField: "Authorization")
-            request.httpMethod = "DELETE"
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                //print("->  request 🤡: ", request as Any)
-                //print("->  respuesta Status Code: ", response as Any)
-                //print("->  error: ", error as Any)
-                guard let allData = data else { return }
-                let outputStr  = String(data: allData, encoding: String.Encoding.utf8) as String?
-                //print("-->✅  Response ->  ", outputStr as Any)
-                if error != nil {
-                    print("Hubo un error")
-                    return
-                }
-                if (response as! HTTPURLResponse).statusCode == 200 {
-                    self.presenter?.responseDeleteColaborador(status: true)
-                }else{
-                    self.presenter?.responseDeleteColaborador(status: false)
-                }
+            if (response as! HTTPURLResponse).statusCode == 200 {
+                self.presenter?.responseDeleteColaborador(status: true)
+            }else{
+                self.presenter?.responseDeleteColaborador(status: false)
+            }
         }
-            task.resume()
-        }
+        task.resume()
+    }
     
 }
